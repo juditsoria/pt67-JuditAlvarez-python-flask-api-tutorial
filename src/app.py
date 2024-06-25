c8c1a457974b24c8e5cc053450fa3d8341c1390c
juditsoria <160534450+juditsoria@users.noreply.github.com>
2024-06-25 17:07:07 +0000

    finish

--- src/app.py
+++ src/app.py
@@ -1,24 +1,27 @@
-from flask  import Flask, jsonify, request
-
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-todos =  [ { "label": "My first task", "done": False } ]
-@app.route('/todos', methods= ['GET'])
+todos = [{"label": "My first task", "done": False}]
 
+@app.route('/todos', methods=['GET'])
 def get_todos():
     return jsonify(todos)
 
-
 @app.route('/todos', methods=['POST'])
-
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
     todos.append(request_body)
+    return jsonify(todos)
 
-    return 'Response for the POST todo'
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    if position < len(todos):
+        todos.pop(position)
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Position out of range"}), 404
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
